948d779c1bdb28fbe311efebc1660dcf5a961b9f
tuana <tuananh171312@outlook.com>
2025-06-18 22:00:45 +0700

    refactor: temporarily remove auth middleware from order routes

--- chotuananhne/src/routes/order.route.js
+++ chotuananhne/src/routes/order.route.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/order.controller");
-const { protect } = require("../middlewares/auth.middleware");
 
-// Create new order
-router.post("/", protect, orderController.createOrder);
+// Temporary fix: Remove auth middleware for now
+// const { protect } = require("../middlewares/auth.middleware");
 
-// Get order by reference
-router.get("/:orderRef", protect, orderController.getOrderByRef);
+// Create new order - removed auth temporarily
+router.post("/", orderController.createOrder);
+
+// Get order by reference - removed auth temporarily
+router.get("/:orderRef", orderController.getOrderByRef);
 
 // Get payment status for order
 router.get("/payment-status/:orderRef", orderController.getPaymentStatus);
 
-// Get user's orders
-router.get("/", protect, orderController.getUserOrders);
+// Get user's orders - removed auth temporarily
+router.get("/", orderController.getUserOrders);
 
 module.exports = router;
